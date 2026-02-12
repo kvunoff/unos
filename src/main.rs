ff54df7630c949ef4b276b34e9db9038290cdbad
kvunoff <citer4709@gmail.com>
2026-02-12 21:40:36 +0400

    Added using serial macro

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -27,9 +27,9 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
