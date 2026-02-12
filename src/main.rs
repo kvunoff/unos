8467add1a81416686bd3aceff46188beb6634674
kvunoff <citer4709@gmail.com>
2026-02-12 15:42:16 +0400

    Remove HELLO static

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
