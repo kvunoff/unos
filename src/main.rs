158d015b90820253b78cc0fdeeae3f45521a8a45
kvunoff <citer4709@gmail.com>
2026-02-12 19:03:25 +0400

    Added testing features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod vga_buffer;
 use crate::vga_buffer::Color;
 
@@ -11,9 +14,28 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println_color!(Color::Red, "Hello World{}", "!");
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
