9dfbdee7eeaf21d27b13b08b4f28a4bca90a86cd
kvunoff <citer4709@gmail.com>
2026-02-12 21:48:18 +0400

    Updated panic_handler

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,19 @@ use crate::vga_buffer::Color;
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
