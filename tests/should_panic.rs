46baa476abb7063fe6a6c563f405f2ffd08a44fe
kvunoff <citer4709@gmail.com>
2026-02-13 11:06:32 +0400

    Add should_fail test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use unos::{QemuExitCode, exit_qemu, serial_println};
+use unos::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -19,6 +19,12 @@ fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
     loop {}
+}
+
+#[test_case]
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
 }
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
