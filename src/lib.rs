413a280c58643db62ee1e2d02e5b37909b60e0cd
kvunoff <citer4709@gmail.com>
2026-02-13 18:23:52 +0400

    Add GDT module

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
+pub mod gdt;
 
 use core::panic::PanicInfo;
 
@@ -19,6 +20,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
