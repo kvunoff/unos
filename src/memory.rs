26c246db7f049c7dc204c1d5f146ffe847d02ff6
kvunoff <citer4709@gmail.com>
2026-02-15 17:39:47 +0400

    Upd FrameAllocator

--- src/memory.rs
+++ src/memory.rs
@@ -27,12 +27,13 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
 
 pub struct EmptyFrameAllocator;
 
-unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        None
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
     }
 }
-
 pub struct BootInfoFrameAllocator {
     memory_map: &'static MemoryMap,
     next: usize,
